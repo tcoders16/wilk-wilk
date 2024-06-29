e17fd3a535ec0f2ddadca348b76fd0ff1b74b622
tcoders16 <166409459+tcoders16@users.noreply.github.com>
2024-06-29 00:00:04 -0400

    Update page.jsx

--- src/app/components/DetailInformation/page.jsx
+++ src/app/components/DetailInformation/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "../ui/TracingBeam";
 
-const DetailInformation() {
+const DetailInformation =()=> {
   return (
       <div className="max-w-2xl mx-auto antialiased pt-4 relative overflow-auto">
         {dummyContent.map((item, index) => (
