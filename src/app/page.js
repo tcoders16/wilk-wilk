73cb81de02a025541b95fb3860da621ccd29680e
tcoders16 <166409459+tcoders16@users.noreply.github.com>
2024-06-29 05:38:45 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 "use client"
 import HelloComponent from './components/HelloComponent/page'; //Animated Hello
 import HeroScrollBrandName from './components/HeroScrollBrandName/page';// Branding and brand name
-import { LocationCard } from './components/LocationCard/page';
+import  LocationCard  from './components/LocationCard/page';
 import ToggleButton from './components/ToggleButton/page';
 import  DetailInformation  from './components/DetailInformation/page';
 import People from './components/People/page';
-import { PlaceholdersAndVanishInput } from './components/ui/placeholders-and-vanish-input';
-import { SparklesPreview } from './components/SparkelsPreview/page';
+import  PlaceholdersAndVanishInput  from './components/ui/placeholders-and-vanish-input';
+import SparklesPreview from './components/SparkelsPreview/page';
 import FlippingText from './components/FlippingText/page';
 import SignupForm from './components/SignupForm/page';
 import  GlobeComponent  from './components/GlobeComponent/page';
 import  SocialMedia  from './components/SocialMedia/page';
-import { TracingBeam } from './components/ui/TracingBeam';
+import  TracingBeam  from './components/ui/TracingBeam';
 
 
 
